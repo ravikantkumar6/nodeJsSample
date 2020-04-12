fa689fc19572f736997130cc8f97539431959fe5
Kumar <RKumar18@ad.harman.com>
2020-04-12 20:05:34 +0530

    access query param or transfer data from one page to another

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testApi = require('./routes/testapi');
+// var userdetail = require('./routes/usedetail');
 
 
 var app = express();
@@ -26,6 +27,7 @@ var router = express.Router();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testApi);
+// app.use('/test', testApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-
 //router.use('/static', express.static('public'));
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+  res.render('userdetail', { title: 'UserDetail', username: req.body.username, password:req.body.password });
 });
 
 module.exports = router;
